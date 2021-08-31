004c47a1090051f2244922117c985a8d74cf8ee4
shubhankarthapa <shubhankarthapa.121@gmail.com>
2021-08-31 19:25:50 +0545

    first commit

---
+++ part1.py
@@ -0,0 +1,34 @@
+board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
---
+++ solver_backtracking.py
@@ -0,0 +1,82 @@
+SIZE = 9
+#sudoku problem
+#cells with value 0 are vacant cells
+matrix = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+#function to print sudoku
+def print_sudoku():
+    for i in matrix:
+        print (i)
+
+
+def number_unassigned(row, col):
+    num_unassign = 0
+    for i in range(0,SIZE):
+        for j in range (0,SIZE):
+            #cell is unassigned
+            if matrix[i][j] == 0:
+                row = i
+                col = j
+                num_unassign = 1
+                a = [row, col, num_unassign]
+                return a
+    a = [-1, -1, num_unassign]
+    return a
+
+def is_safe(n, r, c):
+    #checking in row
+    for i in range(0,SIZE):
+        #there is a cell with same value
+        if matrix[r][i] == n:
+            return False
+    #checking in column
+    for i in range(0,SIZE):
+        #there is a cell with same value
+        if matrix[i][c] == n:
+            return False
+    row_start = (r//3)*3
+    col_start = (c//3)*3;
+    #checking submatrix
+    for i in range(row_start,row_start+3):
+        for j in range(col_start,col_start+3):
+            if matrix[i][j]==n:
+                return False
+    return True
+
+
+def solve_sudoku():
+    row = 0
+    col = 0
+
+    a = number_unassigned(row, col)
+    if a[2] == 0:
+        return True
+    row = a[0]
+    col = a[1]
+    #number between 1 to 9
+    for i in range(1,10):
+        #if we can assign i to the cell or not
+        #the cell is matrix[row][col]
+        if is_safe(i, row, col):
+            matrix[row][col] = i
+            #backtracking
+            if solve_sudoku():
+                return True
+        
+            matrix[row][col]=0
+    return False
+
+if solve_sudoku():
+    print_sudoku()
+else:
+    print("No solution")
